e083f4078a05d7e949bbe144dca5a8052fdf3b3d
AVII921 <khairnaravinash33@gmail.com>
2025-09-05 17:52:05 +0530

    DEMO

---
+++ array1.java
@@ -0,0 +1,29 @@
+import java. util.Scanner;
+class array1{
+	public  static void main (String []args ){
+		Scanner sc= new Scanner ( System.in);
+		System.out.println( "enter a subject  first letear");
+		 char  subjectinitial = sc.next().charAt(0);
+		 int  i;
+		 System.out.println(" " + subjectinitial);
+		 
+			
+		int [] marks = new  int [3];
+		marks[0]=65 ;
+		marks[1]=75;
+		marks[2]=85;
+		
+		if ( i=m){
+		System.out.println(" 1:" +marks[0]);
+		}
+		
+		else if ( i=e)
+		{
+		System.out.println(" 2:"+marks[1]);
+		}
+		else {
+	    System.out.println(" 3:"+marks[2]);
+		}
+		
+	}
+}
