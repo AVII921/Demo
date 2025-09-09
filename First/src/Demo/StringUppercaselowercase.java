a2362cfa1ba70994979c93f0550ecb60f559dad5
AVII921 <khairnaravinash33@gmail.com>
2025-09-09 12:59:45 +0530

    add Stringuppercaselowercase.java

---
+++ First/src/Demo/StringUppercaselowercase.java
@@ -0,0 +1,12 @@
+package Demo;
+
+public class StringUppercaselowercase {
+
+	public static void main(String[] args) {
+		 String str=new String ("avinashkhairnar");
+		 
+		  System.out.println( str.toUpperCase());
+
+	}
+
+}
