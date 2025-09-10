73b0cfd143a5bcb4d9cc5d31c493d4b28ea9eef2
AVII921 <khairnaravinash33@gmail.com>
2025-09-10 23:37:04 +0530

    polindrome

--- First/src/Demo/polindrome.java
+++ First/src/Demo/polindrome.java
@@ -1,9 +1,21 @@
 package Demo;
 
 public class polindrome {
+			public static void main(String[] args) {
+	        String input = "nayan";
+	        System.out.println(input + " is a palindrome? " + isPalindrome(input));
+	    }
 
-	public static void main(String[] args) {
-		
-	}
 
+public static boolean isPalindrome(String s) {
+    int i = 0, j = s.length() - 1;
+    while (i < j) {
+        if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
 }
