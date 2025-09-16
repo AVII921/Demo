2f3765182c28e338b7bf610088a7478c013e7a8a
AVII921 <khairnaravinash33@gmail.com>
2025-09-16 21:25:49 +0530

    update palindrom2.ava

--- First/src/Demo/Palindrome2.java
+++ First/src/Demo/Palindrome2.java
@@ -1,13 +1,13 @@
 package Demo;
 
-public class Palindrome2 {
+public class Palindrome2{
 
 	public static void main(String[] args) {
 		
-		String str="NAYANN";
+		String str="NAYAN";
 		String reversed="null";
 		StringBuilder sd=new StringBuilder(str);
-		 System.out.println(sd);
+		 System.out.print(sd+"   is a palindrome ");
 		 
 		StringBuilder sb2=new StringBuilder(sd.reverse());
 		 reversed=sb2.toString();
