0ce7ce5873f12484444dac2641f32b43a7409afc
AVII921 <khairnaravinash33@gmail.com>
2025-09-16 21:24:32 +0530

    add C_S_M_Hours.java and update palindom2.java

---
+++ First/src/Demo/C_S_M_Hours.java
@@ -0,0 +1,17 @@
+package Demo;
+import java.util.Scanner;
+public class C_S_M_Hours {
+
+	public static void main(String[] args) {
+		System.out.println("Enter  A Seconds");
+		 Scanner sc=new Scanner(System.in);
+		 int sec = sc.nextInt();
+		 int mini = (sec % 3600)/60;
+		 int hours= sec / 3600;
+		 System.out.println(" Time is "+hours+"Hours  "+mini+"Minutes  "+sec+"seconds");
+		 
+	}
+}
+		
+		 
+		
